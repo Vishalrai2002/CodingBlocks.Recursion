2ea02e7c0ada8d5a523c8020db23c5c946415eb4
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2021-12-05 22:56:22 +0530

    Add files via upload

---
+++ 109.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+// Print spelling in reverse order using recursion.
+using namespace std;
+
+string map[] = {"zero", "one", "two", "three", 
+                "four", "five", "six", "seven",
+                "eight", "nine"};
+
+void f1(int n) {
+	// base case
+	if(n == 0) {
+		// you've already printed the spelling of all the digits
+		// in the given number n.
+		cout << endl;
+		return;
+	}
+
+	// recursive case
+
+	// 1. extract the digit at the one's place
+	int d = n%10;
+	// 2. map the digit to its corresponding spelling
+	cout << map[d] << " ";
+	// 3. ask your friend to print the spelling of n/10 in reverse
+	f1(n/10);
+}
+
+void f2(int n) {
+	// base case
+	if(n == 0) {
+		// you've already printed the spelling of all the digits
+		// in the given number n.
+		cout << endl;
+		return;
+	}
+
+	// recursive case
+
+	// 1. ask your friend to print the spelling of n/10
+	f2(n/10);
+	// 2. extract the digit at the one's place
+	int d = n%10;
+	// 3. map the digit to its corresponding spelling
+	cout << map[d] << " ";
+	
+}
+
+int main() {
+
+	int n = 123;
+
+	f1(n);
+	f2(n);
+
+	return 0;
+}
