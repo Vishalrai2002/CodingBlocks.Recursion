210e5d47320f951053a62ac7f6d960e044452638
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2021-12-12 18:58:36 +0530

    Update 119.cpp

--- 119.cpp
+++ 119.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<set>
-// All about set container 
+// All about set container
 using namespace std;
 
 int main() {
@@ -13,40 +13,40 @@ int main() {
 	s.insert(2);
 	s.insert(1);
 
-	cout << "size : " << s.size() << endl;
+	cout << "size : " << s.size() << endl;  // 5
 
 	s.erase(5);
 
-	cout << "size : " << s.size() << endl;
+	cout << "size : " << s.size() << endl;  // 4
 
 	for(auto it=s.begin(), end=s.end(); it != end; it++) {
-		cout << *it << " ";
+		cout << *it << " "; // 1 2 3 4
 	}
 
 	cout << endl;
 
 	for(auto x : s) {
-		cout << x << " ";
+		cout << x << " ";   // 1 2 3 4
 	}
 
 	cout << endl;
 
 	if(s.find(3) != s.end()) {
-		cout << "3 is present" << endl;
+		cout << "3 is present" << endl;  // 3 is present
 	} else {
-		cout << "3 is absent" << endl;
+		cout << "3 is absent" << endl;  
 	}
 
 	if(s.count(5)) {
 		cout << "5 is present" << endl;
 	} else {
-		cout << "5 is absent" << endl;
+		cout << "5 is absent" << endl;   // 5 is absent
 	}
 
 
 	s.clear();
 
-	cout << "size : " << s.size() << endl;
+	cout << "size : " << s.size() << endl;   // size : 0
 
 	return 0;
 }
